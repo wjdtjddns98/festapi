608ed03723bcf3d683b6d36414f134a2e9e22fcb
wjdtjddns98 <wjdtjddns98@naver.com>
2025-07-07 18:33:46 +0900

    초기설정

--- main.py
+++ main.py
@@ -1,13 +1,12 @@
 from fastapi import FastAPI
-
 app = FastAPI()
 
 
 @app.get("/")
-async def root():
+async def root() -> dict[str, str]:
     return {"message": "Hello World"}
 
 
 @app.get("/hello/{name}")
-async def say_hello(name: str):
+async def say_hello(name: str) -> dict[str, str]:
     return {"message": f"Hello {name}"}
