362fced7b4351be4597646e5331bad4ecdc6bb7a
wjdtjddns98 <wjdtjddns98@naver.com>
2025-07-10 16:25:07 +0900

    base62 uuid 추가

---
+++ app/utils/base62.py
@@ -0,0 +1,21 @@
+import string
+from typing import Final
+
+
+class Base62:
+    BASE: Final[str] = string.ascii_letters + string.digits
+    BASE_LEN: Final[int] = len(BASE)
+
+    @classmethod
+    def encode(cls, num: int) -> str:
+        if num < 0:
+            raise ValueError(f"{cls}.encode() needs positive integer but you passed: {num}")
+
+        if num == 0:
+            return cls.BASE[0]
+
+        result = []
+        while num:
+            num, remainder = divmod(num, cls.BASE_LEN)
+            result.append(cls.BASE[remainder])
+        return "".join(result)
